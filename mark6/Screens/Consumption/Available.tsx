c636df82c154cf96975a11c5e0c56545560d0b24
Sandali Disara <sandalidissanayake123@gmail.com>
2024-08-27 21:08:13 +0530

    Add past consumption view barGraph

--- mark6/Screens/Consumption/Available.tsx
+++ mark6/Screens/Consumption/Available.tsx
@@ -1,27 +1,39 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import TankLevel from '../../components/AvailableTank/TankLevel';
 import DailyConsumption from '../../components/Consumption/DaillyConsumption';
+import PastConsumption from '../../components/Graph/PastConsumption';
+
+const testData = [
+    { date: '20 Aug', consumption: 10 },
+    { date: '21 Aug', consumption: 15 },
+    { date: '22 Aug', consumption: 7 },
+    { date: '23 Aug', consumption: 20 },
+    { date: '24 Aug', consumption: 13 },
+    { date: '25 Aug', consumption: 18 },
+    { date: '26 Aug', consumption: 12 },
+];
 
 export default function AvailableScreen() {
- 
     return (
-        <View style={styles.container}>
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
             <View style={styles.tankLevelContainer}>
                 <TankLevel />
             </View>
             <View style={styles.consumptionContainer}>
                 <DailyConsumption date="27th Aug" amount={12} />
             </View>
-            
-        </View>
+            <View style={styles.pastConsumptionContainer}>
+                <PastConsumption data={testData} />
+            </View>
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'flex-start',
+    scrollContainer: {
+        flexGrow: 1,
+        justifyContent: 'flex-start', 
         alignItems: 'center',
         backgroundColor: '#f5f5f5',
         paddingVertical: 20,
@@ -32,8 +44,12 @@ const styles = StyleSheet.create({
         marginBottom: 30,
     },
     consumptionContainer: {
-        width: '100%',
-        alignItems: 'center',
+        width: '100%', 
+        alignItems: 'center', 
     },
-    
+    pastConsumptionContainer: {
+        width: '100%', 
+        alignItems: 'center', 
+        marginTop: 20, // Add some space between DailyConsumption and PastConsumption
+    },
 });
